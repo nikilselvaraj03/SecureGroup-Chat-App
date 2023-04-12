40a4eff31e2ffa72ad2aef96c7ec8d99155407c1
nikilselvaraj03 <nikilselvaraj03@gmail.com>
2023-04-11 19:15:16 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import LoginScreen from './screens/LoginScreen';
+// import RegistrationScreen from './screens/RegistrationScreen';
+// import Home from './screens/Home'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Caveat': require('./assets/fonts/Caveat-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
+        {/* <Stack.Screen options={{headerShown:false}} name="Register" component={RegistrationScreen} /> */}
+        {/* <Stack.Screen options={{headerShown:false}} name="Home" component={Home} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
